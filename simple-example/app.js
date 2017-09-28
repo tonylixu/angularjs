552a2f0d4540636f38eeacfca322b02a3e0a15ff
tonylixu@gmail.com <tonylixu@gmail.com>
2017-09-28 17:50:26 -0400

    Created a single page app

--- simple-example/app.js
+++ simple-example/app.js
@@ -1,10 +1,32 @@
-// MODULE
-var angularApp = angular.module('angularApp', []);
+ // MODULE
+var angularApp = angular.module('angularApp', ['ngMessages', 'ngResource', 'ngRoute']);
+
+angularApp.config(function ($routeProvider) {
+
+    $routeProvider
+    
+    .when('/', {
+        templateUrl: 'pages/main.html',
+        controller: 'mainController'
+    })
+    
+    .when('/second/:num', {
+        templateUrl: 'pages/second.html',
+        controller: 'secondController'
+    })
+    
+});
+
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', function ($scope) {
-    
+// angularjs file define an object called scope service: $scope
+// Aff angularjs services start with $
+// $scope become the middle piece of view and controller
+angularApp.controller('mainController', ['$scope', '$log', '$routeParams', function($scope, $log, $routeParams) {
+    $scope.name = 'Main';
 }]);
 
-
-
+angularApp.controller('secondController', ['$scope', '$log', '$routeParams', function($scope, $log, $routeParams) {
+    $scope.name = 'Second';
+    $scope.num = $routeParams.num;
+}]);
