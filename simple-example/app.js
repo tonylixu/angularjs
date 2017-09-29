b42ff08f87d6f9f596a84209246b96e8038cf6e3
tonylixu@gmail.com <tonylixu@gmail.com>
2017-09-29 10:22:03 -0400

    Defined a self-created service

--- simple-example/app.js
+++ simple-example/app.js
@@ -22,16 +22,35 @@ angularApp.config(function ($routeProvider) {
     
 });
 
+angularApp.service('nameService', function() {
+
+    var self = this;
+    this.name = 'Tony xu';
+
+    this.namelength = function() {
+
+        return self.name.length;
+
+    };
+
+});
 
 // CONTROLLERS
 // angularjs file define an object called scope service: $scope
 // Aff angularjs services start with $
 // $scope become the middle piece of view and controller
-angularApp.controller('mainController', ['$scope', '$log', '$routeParams', function($scope, $log, $routeParams) {
+angularApp.controller('mainController', ['$scope', '$log', '$routeParams', 'nameService', function($scope, $log, $routeParams, nameService) {
     $scope.name = 'Main';
+
+    $log.main = 'Property from main';
+    $log.log(nameService.name);
+    $log.log(nameService.namelength());
+
 }]);
 
 angularApp.controller('secondController', ['$scope', '$log', '$routeParams', function($scope, $log, $routeParams) {
     $scope.name = 'Second';
     $scope.num = $routeParams.num || 1;
+    $log.second = 'Property from second';
+    $log.log($log);
 }]);
